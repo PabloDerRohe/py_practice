dd37330901f365287a1fab8b1d6e3080aaed2859
Pablo Marelli <pablomarelli2@gmail.com>
2022-02-09 17:08:37 -0300

    Generador de personaje de rol

--- py_practice.py
+++ py_practice.py
@@ -1,3 +1,73 @@
-print('Hello world!')
+import random
 
-print('Hola Luna, hola Fili!')
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1,dado2,dado3,dado4)
+
+res = suma - menor
+
+print("Fuerza %i"% suma)
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1,dado2,dado3,dado4)
+
+res = suma - menor
+
+print("Destreza %i"% suma)
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1,dado2,dado3,dado4)
+
+res = suma - menor
+
+print("Constitucion %i"% suma)
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1,dado2,dado3,dado4)
+
+res = suma - menor
+
+print("Inteligencia %i"% suma)
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1,dado2,dado3,dado4)
+
+res = suma - menor
+
+print("Sabiduria %i"% suma)
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min(dado1,dado2,dado3,dado4)
+
+res = suma - menor
+
+print("Carisma %i"% suma)
